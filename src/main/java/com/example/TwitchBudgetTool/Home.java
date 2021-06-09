53b6f4a44b3001a06c4f82bdaf0c93cf0dd3141b
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-06-09 16:20:49 -0500

    FiXED CSS

--- src/main/java/com/example/TwitchBudgetTool/Home.java
+++ src/main/java/com/example/TwitchBudgetTool/Home.java
@@ -12,3 +12,4 @@ public class Home {
         return "home";
     }
 }
+
---
+++ src/main/java/com/example/TwitchBudgetTool/Test.java
@@ -0,0 +1,11 @@
+package com.example.TwitchBudgetTool;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Test {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
