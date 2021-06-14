2b1d31fb3c7d3c8e728481b4bbffcd1d5be57f21
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-06-14 16:06:23 -0500

    Users can sign up, sign in is broken

--- src/main/java/com/example/TwitchBudgetTool/UserRepo.java
+++ src/main/java/com/example/TwitchBudgetTool/UserRepo.java
@@ -1,7 +1,10 @@
 package com.example.TwitchBudgetTool;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface UserRepo extends JpaRepository<Users, Long>{
+    @Query("SELECT u FROM Users u WHERE u.email = ?1")
+    public Users findByEmail(String email);
 }
